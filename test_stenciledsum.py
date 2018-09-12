f84a5304329d7b644b3bf3239111403d943eef63
nicholas-sherer <17535741+nicholas-sherer@users.noreply.github.com>
2018-09-12 16:35:47 -0500

    refactored test code to work with pytest and cleaned up code a litle

--- test_stenciledsum.py
+++ test_stenciledsum.py
@@ -5,11 +5,38 @@ Created on Wed Sep 12 12:26:48 2018
 @author: nashere2
 """
 
+import pytest
 import numpy as np
 import stenciledsum as stsum
 
 
-def unstenciled_sum(big_array, axes, checks=True):
+def random_array(ndim_max, shape_max):
+    ndim = np.random.randint(2, ndim_max)
+    shape = np.random.randint(2, shape_max, size=ndim)
+    return np.random.randint(100, size=shape)
+
+
+def random_axes(array):
+    n_axes = np.random.randint(1, array.ndim+1)
+    axes = np.random.choice(np.arange(n_axes), size=n_axes, replace=False)
+    return axes
+
+
+def random_stencil(array, axes_to_sum, min_shift, max_shift):
+    axes_shape = np.array(array.shape)[np.array(axes_to_sum)]
+    stencil_shape = stsum.correct_stencil_shape(array.ndim, axes_to_sum,
+                                                axes_shape)
+    return np.random.randint(min_shift, max_shift, size=stencil_shape,
+                             dtype='int_')
+
+
+def random_array_and_axes_to_sum(ndim_max, shape_max):
+    array = random_array(ndim_max, shape_max)
+    axes_to_sum = random_axes(array)
+    return array, axes_to_sum
+
+
+def unstenciled_sum(big_array, axes):
     '''Sum big_array across axes.
 
     Should return a result equivalent to calling
@@ -20,31 +47,41 @@ def unstenciled_sum(big_array, axes, checks=True):
     return stsum.stenciled_sum(big_array, axes, stencil)
 
 
-def test_equality_summation(big_array, axes, verbose=False):
+@pytest.mark.parametrize("big_array,axes", [random_array_and_axes_to_sum(7, 7)
+                                            for i in range(1000)])
+def test_equality_of_summation(big_array, axes, verbose=False):
     '''Test equality of unstenciled sum and np.sum with same arguments.'''
     if verbose:
         print('summation using unstenciled_sum is',
               unstenciled_sum(big_array, axes))
         print('summation using np.sum is',
               np.sum(big_array, axis=tuple(axes)))
-    return np.all(np.sum(big_array, axis=tuple(axes)) ==
+    assert np.all(np.sum(big_array, axis=tuple(axes)) ==
                   unstenciled_sum(big_array, axes))
 
 
-def random_eq_test(verbose=False):
-    '''Randomized testing of stenciled sum.'''
-    ndim = np.random.randint(2, 7)
-    shape = np.random.randint(2, 7, size=ndim)
-    n_axes_sum = np.random.randint(1, ndim+1)
-    axes_sum = np.random.choice(np.arange(n_axes_sum),
-                                size=n_axes_sum, replace=False)
-    array = np.random.randint(100, size=shape)
-    if verbose:
-        print('shape is', shape)
-        print('axes summed over is', axes_sum)
-        print('array is', array)
-    try:
-        success = test_equality_summation(array, axes_sum, verbose)
-    except IndexError:
-        success = False
-    return success, array, axes_sum
+def random_complete_reduction(big_array, verbose=False):
+    axes_summed = []
+    stencils = []
+    reduced_arrays = []
+    remaining_array = big_array
+    while remaining_array.size > 1:
+        axes_to_sum = random_axes(remaining_array)
+        axes_summed.append(axes_to_sum)
+        stencil = random_stencil(remaining_array, axes_to_sum, -4, 4)
+        stencils.append(stencil)
+        if verbose is True:
+            print(remaining_array)
+            print(axes_to_sum)
+            print(stencil)
+        remaining_array = stsum.stenciled_sum(remaining_array, axes_to_sum,
+                                              stencil)
+        reduced_arrays.append(remaining_array)
+    return axes_summed, stencils, reduced_arrays
+
+
+@pytest.mark.parametrize("array", [random_array(7, 7) for i in range(100)])
+def test_complete_reduction(array):
+    assert random_complete_reduction(array)[2][-1] == np.sum(array)
+
+pytest.main()
