701854f9b667553aed1bb43c6f4ba808856e79c1
nicholas-sherer <17535741+nicholas-sherer@users.noreply.github.com>
2018-10-25 16:49:09 -0500

    wrote a simplified function for calculating wright fisher probabilites. improved code slightly

--- wrightfisher.py
+++ wrightfisher.py
@@ -18,21 +18,26 @@ def wright_fisher_probabilities(pop_dist, fitnesses):
         pop_dist/np.sum(pop_dist)
 
 
+def _wright_fisher_probabilities(pop_dist, fitnesses):
+    x = pop_dist*np.exp(fitnesses)
+    return x/np.sum(x)
+
+
 # due to a lack of error handling in np.random.multinomial this will silently
 # return very wrong results if N exceeds about 10^9
 def wright_fisher_fitness_update(pop_dist, fitnesses):
-    probabilities = wright_fisher_probabilities(pop_dist, fitnesses)
+    probabilities = _wright_fisher_probabilities(pop_dist, fitnesses)
     shape = probabilities.shape
-    probabilities.shape = np.product(np.array(shape))
+    probabilities.shape = probabilities.size
     next_generation = np.random.multinomial(np.sum(pop_dist), probabilities)
     next_generation.shape = shape
     return next_generation
 
 
 def wright_fisher_fitness_update_bigN(pop_dist, fitnesses, checks=True):
-    probabilities = wright_fisher_probabilities(pop_dist, fitnesses)
+    probabilities = _wright_fisher_probabilities(pop_dist, fitnesses)
     shape = probabilities.shape
-    probabilities.shape = np.product(np.array(shape))
+    probabilities.shape = probabilities.size
     next_generation = am.multinomial_int64(np.sum(pop_dist),
                                            probabilities, checks)
     next_generation.shape = shape
