9afac0477ffe42e06731891fa2e131cc45901da2
nicholas-sherer <17535741+nicholas-sherer@users.noreply.github.com>
2018-10-26 10:26:45 -0500

    deleted some unused functions

--- wrightfisher.py
+++ wrightfisher.py
@@ -9,16 +9,7 @@ import numpy as np
 import arraymultinomial as am
 
 
-def mean_exp_fitness(pop_dist, fitnesses):
-    return np.sum(pop_dist*np.exp(fitnesses))/np.sum(pop_dist)
-
-
 def wright_fisher_probabilities(pop_dist, fitnesses):
-    return np.exp(fitnesses)/mean_exp_fitness(pop_dist, fitnesses) * \
-        pop_dist/np.sum(pop_dist)
-
-
-def _wright_fisher_probabilities(pop_dist, fitnesses):
     x = pop_dist*np.exp(fitnesses)
     return x/np.sum(x)
 
@@ -26,7 +17,7 @@ def _wright_fisher_probabilities(pop_dist, fitnesses):
 # due to a lack of error handling in np.random.multinomial this will silently
 # return very wrong results if N exceeds about 10^9
 def wright_fisher_fitness_update(pop_dist, fitnesses):
-    probabilities = _wright_fisher_probabilities(pop_dist, fitnesses)
+    probabilities = wright_fisher_probabilities(pop_dist, fitnesses)
     shape = probabilities.shape
     probabilities.shape = probabilities.size
     next_generation = np.random.multinomial(np.sum(pop_dist), probabilities)
@@ -35,7 +26,7 @@ def wright_fisher_fitness_update(pop_dist, fitnesses):
 
 
 def wright_fisher_fitness_update_bigN(pop_dist, fitnesses, checks=True):
-    probabilities = _wright_fisher_probabilities(pop_dist, fitnesses)
+    probabilities = wright_fisher_probabilities(pop_dist, fitnesses)
     shape = probabilities.shape
     probabilities.shape = probabilities.size
     next_generation = am.multinomial_int64(np.sum(pop_dist),
