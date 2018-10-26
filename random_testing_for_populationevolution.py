f3f95aae469a9799edb0a936fa44528f3f3f52ec
nicholas-sherer <17535741+nicholas-sherer@users.noreply.github.com>
2018-10-26 11:41:31 -0500

    fixed issues with testing save/load code so that random parameters generated are valid

--- random_testing_for_populationevolution.py
+++ random_testing_for_populationevolution.py
@@ -13,10 +13,11 @@ import populationevolution_v5 as popev
 def randomPopulationParameters(N_high, pop_shape):
     f_high = np.random.uniform(0, 1.0)
     mu_low = np.minimum(10**np.random.uniform(-4, -1), .9/1.1**pop_shape[1])
-    pop_dist = np.random.randint(0, N_high, pop_shape)
+    pop_dist = np.int64(10**np.random.uniform(-4, 0, pop_shape)*N_high)
     K = N_high/2*pop_shape[0]*pop_shape[1]
     delta_f = np.random.uniform(.001, .1)
-    mu_multiple = np.random.uniform(1.1, mu_low**(-1/pop_shape[1]))
+    mu_multiple = np.random.uniform(1.1,
+                                    np.minimum(10, mu_low**(-1/pop_shape[1])))
     fraction_beneficial = 10**np.random.uniform(-10, -4)
     fraction_accurate = 10**np.random.uniform(-10, -4)
     fraction_mu2mu = np.random.uniform(.001, .5)
@@ -34,5 +35,5 @@ def testSaveandLoad(test_pop):
             popev.PopulationStore.loadStartFromFile(temp_save_file,
                                                     temp_save_file['times 0 to 2'],
                                                     temp_save_file)
-        print(test_pop_load.population == test_pop)
-        return test_pop, test_pop_load.population
+        test_status = test_pop_load.population == test_pop
+        return test_status, test_pop, test_pop_load.population
