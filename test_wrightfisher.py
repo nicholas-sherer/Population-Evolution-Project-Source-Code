84e869e8797a13a738c725fa54ef3b06605871d3
nicholas-sherer <17535741+nicholas-sherer@users.noreply.github.com>
2018-11-25 17:08:03 -0600

    updated documentation of wright fisher tests and added test of invariance of wright fisher probabilities to translations in fitness space

--- test_wrightfisher.py
+++ test_wrightfisher.py
@@ -12,6 +12,11 @@ import wrightfisher as wf
 
 
 def expected_delta_mean_exp_f(pop_dist, fitnesses):
+    '''
+    The expected change in fitness after one generation of wright-fisher
+    evolution is equal to the variance in exponential fitness in the population
+    divided by the mean exponential fitness of the population.
+    '''
     return (wf.var_exp_fitness(pop_dist, fitnesses) /
             wf.mean_exp_fitness(pop_dist, fitnesses))
 
@@ -34,9 +39,26 @@ def random_pop_and_fitnessses(N_scale, N_dim, f_min, f_max):
                                                     -10,
                                                     10) for i in range(1000)])
 def test_delta_mean_exp_f(pop_dist, fitnesses):
+    '''
+    Test that the change in mean fitness from generation to generation is close
+    to the expectation for the wright fisher model.
+    '''
     scale = (sampled_delta_exp_f(pop_dist, fitnesses) /
              expected_delta_mean_exp_f(pop_dist, fitnesses))
     assert(np.abs(scale-1) < .1)
+
+
+@pytest.mark.parametrize("pop_dist,fitnesses,trans",
+                         [random_pop_and_fitnessses(10**9,
+                                                    np.random.randint(5, 50),
+                                                    -10,
+                                                    10) +
+                          (np.random.uniform(-10**6, 10**6),)
+                          for i in range(1000)])
+def test_f_approximately_translationally_invariant(pop_dist, fitnesses, trans):
+    wf_prob = wf.wright_fisher_probabilities(pop_dist, fitnesses)
+    wf_prob_trans = wf.wright_fisher_probabilities(pop_dist, fitnesses + trans)
+    assert(np.isclose(wf_prob, wf_prob_trans))
 
 
 pytest.main()
