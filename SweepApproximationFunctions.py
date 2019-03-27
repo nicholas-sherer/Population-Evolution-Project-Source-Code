f4b4d2703387f4bd697afcb94bcf362dc9cd19fc
nicholas-sherer <17535741+nicholas-sherer@users.noreply.github.com>
2019-03-27 18:30:09 -0500

    adjusted SweepApproximationFunctions to properly fit WrightFisher population scheme

--- SweepApproximationFunctions.py
+++ SweepApproximationFunctions.py
@@ -5,37 +5,42 @@ Created on Mon Mar 14 14:07:19 2016
 @author: Nicholas Sherer
 """
 
-from __future__ import division
 
+import functools
 import numpy as np
 
 
-def testApproxIneq(M, f_b, f_a, P_mu, K):
+def testApproxIneq(delta_f, M, f_b, f_a, P_mu, K):
     """
     This returns whether or not the parameters chosen fulfill the three
     inequalities that must be satisfied for the mutation followed by sweep
     approximation to the full model to work.
     """
-    M_test = testMIneq(M)
-    P_mu_test = testP_muIneq(M, P_mu)
-    fix_time_test = testFixIneq(M, f_b, f_a, P_mu, K)
-    return M_test and P_mu_test and fix_time_test
+    M_test = M_inequality(M)
+    P_mu_test = P_mu_inequality(M, P_mu)
+    fix_time_test = fixation_ineqality(M, f_b, f_a, P_mu, K)
+    drift_test = delta_f_inequality(delta_f, K)
+    return M_test and P_mu_test and fix_time_test and drift_test
 
 
-def testMIneq(M):
+def M_inequality(M):
     return M > (1 + np.sqrt(5))/2
 
 
-def testP_muIneq(M, P_mu):
+def P_mu_inequality(M, P_mu):
     return (1 - P_mu)*M > 1
 
 
-def testFixIneq(M, f_b, f_a, P_mu, K):
-    x = (1 - 1/M) > (f_a*P_mu + f_b*(1-P_mu)*(M-1)/((1-P_mu)*M-1))*K*np.log(K)
-    return x
+def fixation_ineqality(M, f_b, f_a, P_mu, K):
+    return (1 - 1/M) > \
+        (f_a*P_mu + f_b*(1-P_mu)*(M-1)/((1-P_mu)*M-1))*K*np.log(K)
 
 
-def fixationProbability(N, s):
+def delta_f_inequality(delta_f, K):
+    return delta_f > 2/K
+
+
+def fixation_probability(K, s):
     """
     Computes the classical fixation probability for an invader against a
     wildtype assuming no further mutations and only the only two fitnesses are
@@ -43,61 +48,81 @@ def fixationProbability(N, s):
     problem of float-induced error when dividing small numbers by small
     numbers.
     """
-    if s > 1 / N:
-        return (1 - np.exp(-2*s))/(1 - np.exp(-4*N*s))
-    elif s > -1 / N:
-        return 1 / (2*N)
+    if s > 1 / K:
+        return (1 - np.exp(-2*s))/(1 - np.exp(-2*K*s))
+    elif s > -1 / K:
+        return 1 / K
     else:
         return 0
 
 
 def effectiveFitnessDifference(f_1, f_2, mu_1, mu_2):
-    return f_2 - f_1 + (mu_1 - mu_2)
+    return np.exp(f_2-f_1)*(1-mu_2)/(1-mu_1)-1
 
 
-def powerOfMultiple(mu1, mu2, mu_multiple):
+def powerOfMultiple(mu1, mu2, M):
     """
-    Given mu1, mu2, and mu_multiple, this computes the k such that mu2 = mu2 *
-    mu_multiple^k
+    Given mu1, mu2, and M, this computes the k such that mu2 = mu1 *
+    M^k
     """
-    return np.round(np.log(mu2/mu1)/np.log(mu_multiple))
+    return np.round(np.log(mu2/mu1)/np.log(M))
 
 
-def findN0k(P_mu, M, k):
-    if k == 0:
-        return 1
-    else:
-        return P_mu * M**(k-1)/(M**k - 1) * findN0k(P_mu, M, k-1)
-
-
-def findNl0(P_mu, mu_min, delta_f, l):
+@functools.lru_cache(maxsize=16)
+def findN0l(P_mu, M, l):
     if l == 0:
         return 1
     else:
-        return (1 - P_mu) * mu_min / (l * delta_f) *\
-            findNl0(P_mu, mu_min, delta_f, l-1)
+        return P_mu * M**(l-1)/(M**l - 1) * findN0l(P_mu, M, l-1)
 
 
-def findNlk(P_mu, M, mu_min, delta_f, k, l):
+@functools.lru_cache(maxsize=16)
+def findNk0(P_mu, mu_min, delta_f, k):
     if k == 0:
-        return findNl0(P_mu, mu_min, delta_f, l)
-    elif l == 0:
-        return findN0k(P_mu, M, k)
+        return 1
     else:
-        num_term1 = (1 - P_mu)*M**k*mu_min*findNlk(P_mu, M, mu_min, delta_f, k,
+        return ((1 - P_mu) * mu_min * np.exp(delta_f) /
+                ((1-mu_min)*(np.exp(k*delta_f)-1)) *
+                findNk0(P_mu, mu_min, delta_f, k-1))
+
+@functools.lru_cache(maxsize=256)
+def findNkl(P_mu, M, mu_min, delta_f, k, l):
+    if l == 0:
+        return findNk0(P_mu, mu_min, delta_f, k)
+    elif k == 0:
+        return findN0l(P_mu, M, l)
+    else:
+        num_term1 = M**(l-1)*mu_min*P_mu*findNkl(P_mu, M, mu_min, delta_f, k,
                                                    l-1)
-        num_term2 = P_mu*M**(k-1)*mu_min*findNlk(P_mu, M, mu_min, delta_f, k-1,
+        num_term2 = M**l*mu_min*(1-P_mu)*np.exp(delta_f)* \
+            findNkl(P_mu, M, mu_min, delta_f, k-1,
                                                  l)
-        denom = l*delta_f + (M**k - 1)*mu_min
+        denom = np.exp(k*delta_f)*(1-mu_min) + (M**l*mu_min - 1)
         return (num_term1 + num_term2) / denom
 
 
-def findN00overN(P_mu, M, mu_min, delta_f, kmax, lmax):
-    total = 0
-    for k in range(kmax):
-        for l in range(lmax):
-            total = total + findNlk(P_mu, M, mu_min, delta_f, k, l)
-    return 1 / total
+def findNeq(P_mu, M, mu_min, delta_f, K):
+    Nkl = [[1, findN0l(P_mu, M, 1)],
+           [findNk0(P_mu, mu_min, delta_f, 1), findNkl(P_mu, M, mu_min, delta_f, 1, 1)]]
+    total = 1
+    total_next = np.sum(Nkl)
+    conv = (total_next - total)/total
+    k=1
+    l=1
+    while conv > 1/K:
+        total = total_next
+        last_row_sum = sum(Nkl[-1])
+        last_column_sum = sum(x[-1] for x in Nkl)
+        if last_row_sum > last_column_sum:
+            Nkl.append([findNkl(P_mu, M, mu_min, delta_f, k+1, x) for x in range(l+1)])
+            k = k+1
+        else:
+            for i, x in enumerate(Nkl):
+                x.append(findNkl(P_mu, M, mu_min, delta_f, i, l+1))
+            l = l+1
+        total_next = np.sum(Nkl)
+        conv = (total_next - total)/total
+    return np.array(Nkl)*K/total_next
 
 
 def findTransitionRate(mu_1, mu_2, delta_f, M, f_b, f_a, P_mu, K):
@@ -111,25 +136,21 @@ def findTransitionRate(mu_1, mu_2, delta_f, M, f_b, f_a, P_mu, K):
 
 def findMutatorSweepRate(mu_1, mu_2, delta_f, M, f_b, P_mu, K):
     s = effectiveFitnessDifference(0, delta_f, mu_1, mu_2)
-    p_fix = fixationProbability(K, s)
-    k = powerOfMultiple(mu_1, mu_2, M)
-    N_eff = findN00overN(P_mu, M, mu_1, delta_f, 10, 10) * findN0k(P_mu, M, k)
-    # N_eff = findN0k(P_mu, M, k)
-    temp = f_b * (1 - P_mu) * mu_2 * N_eff * p_fix * K
-    return temp
+    p_fix = fixation_probability(K, s)
+    k = int(powerOfMultiple(mu_1, mu_2, M))
+    N_eff = findNeq(P_mu, M, mu_1, delta_f, K)[0,k]
+    return f_b * (1 - P_mu) * mu_2 * N_eff * p_fix
 
 
 def findAntiMutatorSweepRate(mu_1, mu_2, delta_f, M, f_a, P_mu, K):
     s = effectiveFitnessDifference(0, 0, mu_1, mu_2)
-    p_fix = fixationProbability(K, s)
+    p_fix = fixation_probability(K, s)
     k = powerOfMultiple(mu_1, mu_2, M)
     if k < -1.00:
         return 0
     else:
-        N_eff = findN00overN(P_mu, M, mu_1, delta_f, 10, 10)
-        # N_eff = 1
-        temp = f_a * P_mu * mu_1 * N_eff * p_fix * K
-        return temp
+        N_eff = findNeq(P_mu, M, mu_1, delta_f, K)[0,0]
+        return f_a * P_mu * mu_1 * N_eff * p_fix
 
 
 def findTransitionMatrix(mu_list, delta_f, M, f_b, f_a, P_mu, K):
